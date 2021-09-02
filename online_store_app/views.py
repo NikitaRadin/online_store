cb07752ddb9486621ae43451ef50ca79e1f80d5f
Nikita Radin <NikitaRadin1999@gmail.com>
2021-09-02 17:52:43 +0300

    Добавлены шаблоны для категорий

--- online_store_app/views.py
+++ online_store_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def clothes(request):
+    context = {'subcategories': ['Куртки', 'Джинсы', 'Футболки', 'Шорты', 'Рубашки', 'Брюки', 'Трусы', 'Носки']}
+    return render(request, 'clothes.html', context=context)
+
+
+def shoes(request):
+    context = {'subcategories': ['Ботинки', 'Сандали', 'Туфли', 'Тапки']}
+    return render(request, 'shoes.html', context=context)
