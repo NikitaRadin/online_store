6ba376bd5c15a2ab60f7072ab089e998b9abe562
Nikita Radin <NikitaRadin1999@gmail.com>
2021-09-03 22:28:49 +0300

    Реализовано общее для всех категорий представление

--- online_store/urls.py
+++ online_store/urls.py
@@ -3,7 +3,5 @@ from online_store_app import views
 
 
 urlpatterns = [
-    path('', views.clothes),
-    path('clothes/', views.clothes),
-    path('shoes/', views.shoes)
+    path('category/', views.category)
 ]
--- online_store_app/views.py
+++ online_store_app/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
 
 
-def clothes(request):
-    context = {'title': 'Одежда',
-               'header': 'Одежда',
-               'subcategories': ['Куртки', 'Джинсы', 'Футболки', 'Шорты', 'Рубашки', 'Брюки', 'Трусы', 'Носки']}
-    return render(request, 'category.html', context=context)
-
-
-def shoes(request):
-    context = {'title': 'Обувь',
-               'header': 'Обувь',
-               'subcategories': ['Ботинки', 'Сандали', 'Туфли', 'Тапки']}
+def category(request):
+    category_id = request.GET.get('category_id', None)
+    if category_id == '1':
+        context = {'title': 'Одежда',
+                   'header': 'Одежда',
+                   'subcategories': ['Куртки', 'Джинсы', 'Футболки', 'Шорты', 'Рубашки', 'Брюки', 'Трусы', 'Носки']}
+    elif category_id == '2':
+        context = {'title': 'Обувь',
+                   'header': 'Обувь',
+                   'subcategories': ['Ботинки', 'Сандали', 'Туфли', 'Тапки']}
     return render(request, 'category.html', context=context)
