bd2ff28469cb264dff6dca13813af15a9002f791
Nikita Radin <NikitaRadin1999@gmail.com>
2021-09-03 21:22:48 +0300

    Реализован общий для всех категорий шаблон

--- online_store_app/views.py
+++ online_store_app/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 
 
 def clothes(request):
-    context = {'subcategories': ['Куртки', 'Джинсы', 'Футболки', 'Шорты', 'Рубашки', 'Брюки', 'Трусы', 'Носки']}
-    return render(request, 'clothes.html', context=context)
+    context = {'title': 'Одежда',
+               'header': 'Одежда',
+               'subcategories': ['Куртки', 'Джинсы', 'Футболки', 'Шорты', 'Рубашки', 'Брюки', 'Трусы', 'Носки']}
+    return render(request, 'category.html', context=context)
 
 
 def shoes(request):
-    context = {'subcategories': ['Ботинки', 'Сандали', 'Туфли', 'Тапки']}
-    return render(request, 'shoes.html', context=context)
+    context = {'title': 'Обувь',
+               'header': 'Обувь',
+               'subcategories': ['Ботинки', 'Сандали', 'Туфли', 'Тапки']}
+    return render(request, 'category.html', context=context)
